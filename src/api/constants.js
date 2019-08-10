3fc7c55db6e67a4bfc62a88d6a306c620fc38ecf
Swainstha <swainstha@outlook.com>
2019-08-10 18:00:34 +0545

    changed server url

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://127.0.0.1:8000/api/'
+export const API_URL = 'https://forumbackend.appspot.com/api/'
   // process.env.NODE_ENV === 'production'
   //   ? process.env.REACT_APP_PROD_API_URL
   //   : process.env.REACT_APP_DEV_API_URL;
